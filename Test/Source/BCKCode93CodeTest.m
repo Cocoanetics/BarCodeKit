5ca87e46c76ace28f790e497ed7a7dc7cec406df
Geoff Breemer <gmbreemer@gmail.com>
2013-08-26 21:02:15 +1000

    Added Code93 unit test class

---
+++ Test/Source/BCKCode93CodeTest.m
@@ -0,0 +1,34 @@
+//
+//  BCKCode93CodeTest.m
+//  BarCodeKit
+//
+//  Created by Geoff Breemer on 26/08/13.
+//  Copyright (c) 2013 Oliver Drobnik. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BCKCode93CodeTest : SenTestCase
+
+@end
+
+@implementation BCKCode93CodeTest
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+//    STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
