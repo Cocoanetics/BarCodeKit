4d76344db39c46b7b6a28ac86f90c25a8ba6b25b
Geoff Breemer <gmbreemer@gmail.com>
2013-09-02 22:25:48 +1000

    Added support for Code 39 Modulo 43

---
+++ Core/Source/BCKCode39CodeModulo43.h
@@ -0,0 +1,13 @@
+//
+//  BCKCode39CodeModulo43.h
+//  BarCodeKit
+//
+//  Created by Geoff Breemer on 02/09/13.
+//  Copyright (c) 2013 Oliver Drobnik. All rights reserved.
+//
+
+#import "BCKCode39Code.h"
+
+@interface BCKCode39CodeModulo43 : BCKCode39Code
+
+@end
