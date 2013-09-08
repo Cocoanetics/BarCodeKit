060a7f95d8d145675976681b6b62fe18063f457f
Oliver Drobnik <oliver@cocoanetics.com>
2013-09-08 18:38:42 +0200

    Fixed comments for AppleDoc processing

--- Core/Source/BCKCode93ContentCodeCharacter.h
+++ Core/Source/BCKCode93ContentCodeCharacter.h
@@ -8,6 +8,9 @@
 
 #import "BCKCode93CodeCharacter.h"
 
+/**
+ Specialized subclass of BCKCode93CodeCharacter used to represent content code characters in Code93
+ */
 @interface BCKCode93ContentCodeCharacter : BCKCode93CodeCharacter
 
 /**
@@ -16,12 +19,14 @@
 
 /**
  Initialise a content code character using one of the 43 regular characters or 4 special characters
+ @param character The character to encode
  @returns the content code character
  */
 - (instancetype)initWithCharacter:(NSString *)character;
 
 /**
  Initialise a content code character using its character value. Only supports the 43 regular characters and 4 special characters
+ @param characterValue The character value to encode
  @returns the content code character
  */
 - (instancetype)initWithValue:(NSUInteger)characterValue;
