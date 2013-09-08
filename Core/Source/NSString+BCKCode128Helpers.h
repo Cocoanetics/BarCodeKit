baf69e7849b7759594fb47e6afdfc6c47b46b303
Jaanus Siim <jaanus@jaanussiim.com>
2013-09-08 15:19:52 +0300

    Add control characters

--- Core/Source/NSString+BCKCode128Helpers.h
+++ Core/Source/NSString+BCKCode128Helpers.h
@@ -8,11 +8,22 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ Helper methods for determining next next characters that should be encoded.
+ */
 @interface NSString (BCKCode128Helpers)
 
+/**
+ Check if given string contains only numbers (0-9).
+
+ @returns does given string contain only numbers
+ */
 - (BOOL)containsOnlyNumbers;
+
 - (BOOL)firstTwoCharactersAreNumbers;
+
 - (BOOL)firstFourCharactersAreNumbers;
+
 - (BOOL)firstSixCharactersAreNumbers;
 
 @end
