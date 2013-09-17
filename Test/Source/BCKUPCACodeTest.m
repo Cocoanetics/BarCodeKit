9e50030155bf904d27b61a1d8902e68ccbd8d1ff
Jaanus Siim <jaanus@jaanussiim.com>
2013-09-17 09:09:05 +0300

    UPC-A tests

--- Test/Source/BCKUPCACodeTest.m
+++ Test/Source/BCKUPCACodeTest.m
@@ -6,10 +6,40 @@
 //  Copyright (c) 2013 Oliver Drobnik. All rights reserved.
 //
 
+#import "BCKUPCACode.h"
+
+@interface BCKUPCACode ()
+
+- (NSString *)bitString;
+
+@end
+
 @interface BCKUPCACodeTest : SenTestCase
 
 @end
 
 @implementation BCKUPCACodeTest
+
+- (void)testEncode
+{
+	NSError *error;
+	BCKUPCACode *code = [[BCKUPCACode alloc] initWithContent:@"088345100517" error:&error];
+	STAssertNotNil(code, [error localizedDescription]);
+
+	NSString *expected = @"10100011010110111011011101111010100011011000101010110011011100101110010100111011001101000100101";
+	NSString *actual = [code bitString];
+	BOOL isEqual = [expected isEqualToString:actual];
+
+	STAssertTrue(isEqual, @"Result from encoding incorrect");
+}
+
+// text cannot be encoded
+- (void)testEncodeInvalidText
+{
+	NSError *error;
+	BCKUPCACode *code = [[BCKUPCACode alloc] initWithContent:@"A8834510051B" error:&error];
+	STAssertNil(code, @"Should not be able to encode non-digits in EAN13");
+	STAssertNotNil(error, @"No error message returned");
+}
 
 @end
